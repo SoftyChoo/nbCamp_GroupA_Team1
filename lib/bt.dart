dd229d73dbd13df3080fc685cdc71e131ee0de4e
황진주 <vjinjoov@naver.com>
2023-07-11 19:58:21 +0900

    이미지, 아래설명 4:6flex줌, 술이미지가 들어갈 stack박스 만드는중

--- lib/bt.dart
+++ lib/bt.dart
@@ -21,9 +21,40 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Stack(
-              children: [Image.network('https://ifh.cc/g/XHj5VA.jpg')],
-            )
+            Expanded(
+              flex: 4,
+              child: Stack(
+                alignment: Alignment.centerRight,
+                children: [
+                  Image.network(
+                    'https://ifh.cc/g/XHj5VA.jpg',
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Container(
+                color: Colors.black,
+                child: ListView(
+                  children: [
+                    Center(
+                      child: Text(
+                        'Other Widgets',
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                    ),
+                    // Add more widgets here
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
