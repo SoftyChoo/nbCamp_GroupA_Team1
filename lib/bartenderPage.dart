a6baa559667764bd3fbb80422c143748cb8558de
황진주 <vjinjoov@naver.com>
2023-07-12 16:46:24 +0900

    별점배경임시블루/세부컬러바꾸기전

--- lib/bartenderPage.dart
+++ lib/bartenderPage.dart
@@ -55,11 +55,15 @@ class _bartenderPage extends State<bartenderPage> {
                         width: double.infinity,
                         fit: BoxFit.cover,
                       ),
-                      Container(
-                        width: 150,
-                        height: 200,
-                        color: Colors.black,
-                      ), //칵테일이미지박스
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          margin: EdgeInsets.all(8.0), // 원하는 margin 값을 설정합니다.
+                          width: 120,
+                          height: 180,
+                          color: Colors.black,
+                        ),
+                      ) //칵테일이미지박스
                     ],
                   ),
                 ),
@@ -71,6 +75,16 @@ class _bartenderPage extends State<bartenderPage> {
                   color: Colors.black,
                   child: ListView(
                     children: [
+                      SizedBox(
+                        child: Text(
+                          'INTRODUCTION',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                       Row(
                         children: [
                           SizedBox(
@@ -79,7 +93,7 @@ class _bartenderPage extends State<bartenderPage> {
                             child: Align(
                               alignment: Alignment.centerRight,
                               child: Text(
-                                'NAME',
+                                'NAME : ',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 20,
@@ -103,7 +117,7 @@ class _bartenderPage extends State<bartenderPage> {
                             child: Align(
                               alignment: Alignment.centerRight,
                               child: Text(
-                                'MBTI',
+                                'MBTI : ',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 20,
@@ -127,7 +141,7 @@ class _bartenderPage extends State<bartenderPage> {
                             child: Align(
                               alignment: Alignment.centerRight,
                               child: Text(
-                                'AGE',
+                                'AGE : ',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 20,
@@ -199,81 +213,91 @@ class _bartenderPage extends State<bartenderPage> {
                 flex: 3,
                 child: Stack(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          RatingBar.builder(
-                            initialRating: rating, // rating 변수를 초기 별점 값으로 설정
-                            minRating: 0, //평점 최소 단위. 0점부터 시작
-                            direction: Axis.horizontal,
-                            allowHalfRating: true, //별점 반값 가능 허용여부
-                            itemCount: 5, //평점 갯수 조절
-                            itemSize: 30.0, // 아이콘 크기 조절
-                            itemBuilder: (context, _) => Icon(
-                              Icons.wine_bar,
-                              color: Colors.amber[500],
-                            ),
-                            onRatingUpdate: (newRating) {
-                              setState(() {
-                                rating = newRating; // rating 변수 업데이트
-                              });
-                              print(rating); // 업데이트된 별점 출력
-                            },
-                          ),
-                          TextField(
-                            focusNode: textFocusNode, // FocusNode 할당
-                            scrollPhysics:
-                                NeverScrollableScrollPhysics(), //스크롤 불가
-                            maxLines: 3, //n줄까지 화면에 보임
-                            maxLength: 100, //글자수 제한
-                            decoration: InputDecoration(
-                              hintText: '평가를 남겨주세요',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(0),
+                    Container(
+                      color: Colors.blue, //별점 배경색변경(검정색으로하고싶음)
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              RatingBar.builder(
+                                initialRating:
+                                    rating, // rating 변수를 초기 별점 값으로 설정
+                                minRating: 0, //평점 최소 단위. 0점부터 시작
+                                direction: Axis.horizontal,
+                                allowHalfRating: true, //별점 반값 가능 허용여부
+                                itemCount: 5, //평점 갯수 조절
+                                itemSize: 30.0, // 아이콘 크기 조절
+                                itemBuilder: (context, _) => Icon(
+                                  Icons.wine_bar,
+                                  color: Colors.amber[500],
+                                ),
+                                onRatingUpdate: (newRating) {
+                                  setState(() {
+                                    rating = newRating; // rating 변수 업데이트
+                                  });
+                                  print(rating); // 업데이트된 별점 출력
+                                },
                               ),
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                CommentCompleted = true; // 작성 완료 상태 업데이트
-                              });
-                              textFocusNode.unfocus(); // 텍스트 필드에서 포커스 해제
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    content: Text('작성을 완료하셨습니까?'),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context)
-                                              .pop(); // 대화 상자 닫기
-                                        },
-                                        child: Text(
-                                          '확인',
-                                          style: TextStyle(color: Colors.blue),
-                                        ),
-                                      ),
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context)
-                                              .pop(); // 대화 상자 닫기
-                                        },
-                                        child: Text(
-                                          '취소',
-                                          style: TextStyle(color: Colors.red),
-                                        ),
-                                      ),
-                                    ],
+                              TextField(
+                                focusNode: textFocusNode, // FocusNode 할당
+                                scrollPhysics:
+                                    NeverScrollableScrollPhysics(), //스크롤 불가
+                                maxLines: 3, //n줄까지 화면에 보임
+                                maxLength: 100, //글자수 제한
+                                decoration: InputDecoration(
+                                  hintText: '평가를 남겨주세요',
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(0),
+                                  ),
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(
+                                    () {
+                                      CommentCompleted = true; // 작성 완료 상태 업데이트
+                                    },
+                                  );
+                                  textFocusNode.unfocus(); // 텍스트 필드에서 포커스 해제
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        content: Text('작성을 완료하셨습니까?'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context)
+                                                  .pop(); // 대화 상자 닫기
+                                            },
+                                            child: Text(
+                                              '확인',
+                                              style:
+                                                  TextStyle(color: Colors.blue),
+                                            ),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context)
+                                                  .pop(); // 대화 상자 닫기
+                                            },
+                                            child: Text(
+                                              '취소',
+                                              style:
+                                                  TextStyle(color: Colors.red),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    },
                                   );
                                 },
-                              );
-                            },
-                            child: Text('확인'),
+                                child: Text('확인'),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ],
